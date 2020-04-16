9ba4004eaf5f9309db7da6fa33ff5a84b6a6c175
danilobrinu <ddbn.c2@gmail.com>
2020-04-16 12:29:33 -0500

    Update test

--- test_app.py
+++ test_app.py
@@ -1,26 +1,30 @@
+from flask import json
 from app import app
 
 
 def test_home():
-    res = app.test_client().get("/")
+    with app.test_client() as client:
+        res = client.get("/")
 
-    assert res.status_code == 200
-    assert b"Sklearn Prediction Home" in res.data
+        assert res.status_code == 200
+        assert b"Sklearn Prediction Home" in res.data
 
 
 def test_predict():
-    res = app.test_client().post(
-        "/predict",
-        json={
-            "CHAS": {"0": 0},
-            "RM": {"0": 6.575},
-            "TAX": {"0": 296.0},
-            "PTRATIO": {"0": 15.3},
-            "B": {"0": 396.9},
-            "LSTAT": {"0": 4.98},
-        },
-        follow_redirects=True,
-    )
+    with app.test_client() as client:
+        res = client.post(
+            "/predict",
+            json={
+                "CHAS": {"0": 0},
+                "RM": {"0": 6.575},
+                "TAX": {"0": 296.0},
+                "PTRATIO": {"0": 15.3},
+                "B": {"0": 396.9},
+                "LSTAT": {"0": 4.98},
+            },
+            follow_redirects=True,
+        )
 
-    assert res.status_code == 200
-    assert res.data == "test"
+        assert res.status_code == 200
+        assert len(json.loads(res.data).get("prediction", [])) > 0
+
