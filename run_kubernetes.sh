6dad80f852996d7ac3fdf861872bf3f8f37045e4
danilobrinu <ddbn.c2@gmail.com>
2020-04-16 21:45:18 -0500

    Update run_kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=minorpatch/udacity-cloud-devops-p4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run '$dockerpath-app' --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward '$dockerpath-app' 80:80
